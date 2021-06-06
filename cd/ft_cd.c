ae9c5c43d3fa5ae702145be9238e67852bbf9f04
tmurase-42 <darekamizuw@gmail.com>
2021-06-06 12:16:03 +0900

    ft_is_commandを実装

--- cd/ft_cd.c
+++ cd/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: tmurase <tmurase@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/05 17:01:21 by tmurase           #+#    #+#             */
-/*   Updated: 2021/06/05 19:35:59 by tmurase          ###   ########.fr       */
+/*   Updated: 2021/06/06 12:15:11 by mitchiwak        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,11 @@ t_bool ft_cd(t_command *command_info)
 {
 	t_env	*dir;
 	int		result;
-
+	if (command_info->argc > 2)
+	{
+		dir = ft_env_get("HOME", command_info->envs)
+		
+	}
 	dir = ft_env_get("HOME",command_info->envs);
 	printf("dir->value = %s\n", dir->value);
 	result = chdir(dir->value);
