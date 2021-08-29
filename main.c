62dc32cca069694a9cf3f4728fe1f4b436c7f618
tmurase-42 <darekamizuw@gmail.com>
2021-08-29 11:16:45 +0900

    ft_token_error.cを追加

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tdofuku <tdofuku@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*   By: tmurase <tmurase@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/30 08:55:35 by mitchiwak         #+#    #+#             */
-/*   Updated: 2021/08/05 23:32:35 by tdofuku          ###   ########.fr       */
+/*   Updated: 2021/08/29 11:11:53 by tmurase          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ static	void	run_commandline(int ret, char **command, t_env *envs)
 {
 	t_mshl_data	*mshl_data;
 	t_cmd		*cmd;
-	t_cmd		*current_cmd;
+	t_cmd		*current_cmd = NULL;
 	t_token		*tokens;
 	char		*token_str;
 	// char		*tmp_str;
@@ -46,6 +46,7 @@ static	void	run_commandline(int ret, char **command, t_env *envs)
 
 		// トークンに分離する
 		tokens = ft_lexer(*command);
+	//	ft_token_print(tokens);
 
 		// ;とかの「異常なトークン」の検知とエラー吐き出し
 		// ここは未実装（村瀬さん）
@@ -60,18 +61,17 @@ static	void	run_commandline(int ret, char **command, t_env *envs)
 		while (current_cmd)
 		{
 			// トークンに環境変数展開をかける
-			ft_expand(current_cmd, mshl_data);
+			//ft_expand(current_cmd, mshl_data);
 
 			// トークンを一度文字列に戻す
-			token_str = ft_token_str(current_cmd->args, 0, current_cmd->argc);
-			ft_token_free(current_cmd->args);
+			//token_str = ft_token_str(current_cmd->args, 0, current_cmd->argc);
+			//ft_token_free(current_cmd->args);
 
 			// 再度トークンに分離する（それをcmd構造体に入れる）
-			tokens = ft_lexer(token_str);
-			current_cmd->args = tokens;
+			//tokens = ft_lexer(token_str);
+			//current_cmd->args = tokens;
 
 			// トークン確認用テスト関数
-			// ft_token_print(current_cmd->args);
 
 			// コマンドを実行する
 			ft_execute_command(current_cmd, mshl_data);
