4cb5a8a022821421041ecd8175493796ad2f5d40
Taisei Dofuku <tdofuku@c1r12s7.42tokyo.jp>
2021-09-04 10:51:36 +0900

    Update process waiting function

--- exec/ft_exec_parent_process.c
+++ exec/ft_exec_parent_process.c
@@ -6,7 +6,7 @@
 /*   By: tdofuku <tdofuku@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/01 19:27:55 by tdofuku           #+#    #+#             */
-/*   Updated: 2021/09/01 22:10:52 by tdofuku          ###   ########.fr       */
+/*   Updated: 2021/09/04 09:47:50 by tdofuku          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	ft_exec_parent_process(int new_pipe[], int old_pipe[], t_cmd *cmd,
 {
 	extern t_mshl_data	*g_mshl_data;
 
-	// sigintのセット
+	// sigintを無視する
 	ft_sigint_setter(SIG_IGN);
 	// 新しいパイプを、古いパイプとして保存
 	ft_pipe_update(g_mshl_data->pipe_state, old_pipe, new_pipe);
