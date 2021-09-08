75eea03e38f161800b2d0e59228acebfbfc28cdf
tmurase-42 <darekamizuw@gmail.com>
2021-09-08 21:20:35 +0900

    completed redirect parent prosess version

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: tmurase <tmurase@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/11 06:52:14 by tmurase           #+#    #+#             */
-/*   Updated: 2021/09/07 20:29:35 by tmurase          ###   ########.fr       */
+/*   Updated: 2021/09/08 20:51:12 by tmurase          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -109,6 +109,7 @@ struct 				s_redirect
 	int		left_fd;
 	int		right_fd;
 	int		type;
+	int		is_last;
 	char		*open_filepath;
 	t_redirect	*next;
 	t_redirect	*prev;
@@ -121,6 +122,8 @@ struct			s_cmd
 	int		argc;
 	struct	s_cmd *next;
 	struct 	s_redirect *redirect;
+	int		final_greater_fd;
+	int		final_lesser_fd;
 	pid_t	pid;
 };
 
@@ -262,8 +265,12 @@ void	ft_sigint_setter(void (*func)(int));
 
 /* redirect functions */
 t_bool	ft_setup_redirect(t_cmd	*cmd);
-void	ft_test_print_redirect(t_redirect *redirect);
-void	ft_dup_redirect(t_redirect *redirect);
+void ft_test_print_redirect(t_cmd *cmd);
 t_redirect	*ft_create_redirect(void);
 void	ft_import_redirect_information(t_cmd *cmd, t_token *redirect_token, int default_fd);
-void	ft_dup_heredoc(t_cmd *cmd);
+void	ft_dup_heredoc(t_redirect *redir);
+void	ft_getfd_redirect(t_cmd *cmd);
+t_bool	ft_check_redirect(t_cmd *cmd);
+t_bool	ft_dup_redirect(t_cmd *cmd);
+t_bool	ft_backup_fd(t_cmd *cmd);
+t_bool	ft_delete_tmpfile(t_cmd *cmd, int final_greater_fd);
---
+++ redirect/ft_getfd_redirect.c
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_getfd_redirect.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmurase <tmurase@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/08 14:47:16 by tmurase           #+#    #+#             */
+/*   Updated: 2021/09/08 21:04:13 by tmurase          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+void	ft_getfd_redirect(t_cmd *cmd)
+{
+	t_redirect	*redir;
+
+	redir = cmd->redirect;
+	while (redir)
+	{
+		if (redir->type == CHAR_GREATER)
+		{
+			redir->right_fd = open(redir->open_filepath, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+			cmd->final_greater_fd = redir->right_fd;
+		}
+		else if (redir->type == DOUBLE_GREATER)
+		{
+			redir->right_fd = open(redir->open_filepath, O_RDWR | O_CREAT | O_APPEND, 0777);
+			cmd->final_greater_fd = redir->right_fd;
+		}
+		else if (redir->type == CHAR_LESSER)
+		{
+			redir->right_fd = open(redir->open_filepath, O_RDWR, 0777);
+			cmd->final_lesser_fd = redir->right_fd;
+		}
+		else if (redir->type == DOUBLE_LESSER)
+		{
+			redir->right_fd = open("tmp.txt", O_WRONLY | O_CREAT | O_TRUNC, 0664);
+			cmd->final_lesser_fd = redir->right_fd;
+			ft_dup_heredoc(redir);
+		}
+		redir = redir->next;
+	}
+	//ft_test_print_redirect(cmd);
+}
+
+static	t_bool check_error_open_close(t_redirect *redir, int final_fd)
+{
+	if (redir->right_fd < 0)
+	{
+		ft_error_display("minishell","open", 1);
+		return (FALSE);
+	}
+	if (redir->right_fd != final_fd)
+	{
+		if (close(redir->right_fd) < 0)
+		{
+			ft_error_display("minishell", "close", 1);
+			return (FALSE);
+		}
+		if (redir->type == DOUBLE_LESSER)
+		{
+			if (unlink(redir->open_filepath) < 0)
+			{
+				ft_error_display("minishell", "unlink", 1);
+				return (FALSE);
+			}
+		}
+	}
+	return (TRUE);
+}
+
+t_bool	ft_check_redirect(t_cmd *cmd)
+{
+	t_redirect *redir;
+
+	redir = cmd->redirect;
+	while (redir)
+	{
+		if (redir->type == CHAR_GENERAL || redir->type == DOUBLE_GREATER)
+			if (check_error_open_close(redir, cmd->final_greater_fd) == FALSE)
+				return (FALSE);
+		if (redir->type == CHAR_LESSER || redir->type == DOUBLE_LESSER)
+			if (check_error_open_close(redir, cmd->final_lesser_fd) == FALSE)
+				return (FALSE);
+		redir = redir->next;
+	}
+	return (TRUE);
+}
