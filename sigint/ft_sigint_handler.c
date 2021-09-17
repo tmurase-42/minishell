7417ae5a9070eddddb6ba45d3c73cfed07cca940
Taisei Dofuku <tdofuku@c1r12s7.42tokyo.jp>
2021-09-17 22:55:22 +0900

    norm 対応

--- sigint/ft_sigint_handler.c
+++ sigint/ft_sigint_handler.c
@@ -3,23 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   ft_sigint_handler.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tmurase <tmurase@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*   By: tdofuku <tdofuku@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/30 16:31:41 by tdofuku           #+#    #+#             */
-/*   Updated: 2021/09/14 16:54:05 by tmurase          ###   ########.fr       */
+/*   Updated: 2021/09/17 22:55:13 by tdofuku          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-void ft_sigint_handler(int sig)
+void	ft_sigint_handler(int sig)
 {
 	extern t_mshl_data	*g_mshl_data;
-	int		save_errno;
+	int					save_errno;
 
 	save_errno = errno;
-	//ft_putstr_fd("\033[2D\033[OK", STDERR_FILENO);
-	// ft_putstr_fd("\033[15C", 1);      //この15Cは打ち込んだ文字数によって変わる。
 	ft_putstr_fd("\b\b  \b\b", 1);
 	rl_replace_line("", 0);
 	rl_on_new_line();
