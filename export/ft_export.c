8174715b4825c4746e040123705352a89df94e08
Taisei Dofuku <tdofuku@c1r12s7.42tokyo.jp>
2021-09-20 15:20:48 +0900

    Update export

--- export/ft_export.c
+++ export/ft_export.c
@@ -6,7 +6,7 @@
 /*   By: tdofuku <tdofuku@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/07 19:50:48 by tdofuku           #+#    #+#             */
-/*   Updated: 2021/09/18 01:35:51 by tdofuku          ###   ########.fr       */
+/*   Updated: 2021/09/20 15:14:09 by tdofuku          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 int	ft_export(t_cmd *cmd)
 {
-	if (cmd->argc == 1)
+	if (cmd->args && cmd->args->next)
 	{
-		return (ft_export_print_envs());
+		return (ft_export_set_envs(cmd));
 	}
 	else
 	{
-		return (ft_export_set_envs(cmd));
+		return (ft_export_print_envs());
 	}
 	return (EXIT_SUCCESS);
 }
